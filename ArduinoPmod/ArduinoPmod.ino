6f69942d02e7f14c7cf018720da3dc27cdbaa242
Joni Rapatti <60032253+gitJoni@users.noreply.github.com>
2021-10-08 13:42:00 +0300

    Updated to latest
    Removed Serial ports for now that the arduino can work as a standalone circuit board. Added also possibility to reset the board if the connection to wifi get lost.

--- ArduinoPmod/ArduinoPmod.ino
+++ ArduinoPmod/ArduinoPmod.ino
@@ -1,18 +1,12 @@
 /************************************************************************
-
   Test of the Pmod MIC3
-
 *************************************************************************
-
   Description: Pmod_MIC3
   The sound captured by the module is displayed in the serial plotter.
   You can set the sensitivity of the mic with the potentiometer (srewdriver might be needed).
-
-
   Material
   1. Arduino Uno
   2. Pmod MIC3
-
   Wiring
   Module<----------> Arduino
   VCC     to        5V
@@ -20,7 +14,6 @@
   SCK     to        13 (SCK)
   MISO    to        12 (MISO)
   SS      to        10
-
 ************************************************************************/
 
 #define SS 8 // Assignment of the SS pin MIC sensor
@@ -28,55 +21,75 @@
 
 #include <SPI.h> // call library
 #include <WiFi101.h>
+#include <PubSubClient.h>
 #include <credentials.h>
 #include <ArduinoJson.h>
 
 char ssid[] = SECRET_SSID;
 char passwd[] = SECRET_PSW;
+const char* brokerUser = "use-token-auth";
+const char* brokerPass = ")9ddw&ICbA)!Ps+(_*";
+const char* broker = "ypessw.messaging.internetofthings.ibmcloud.com";
 
-int status = WL_IDLE_STATUS;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 WiFiServer server(80);
 
 void setup() {
-  Serial.begin(9600); // initialization of serial communication
   SPI.begin(); // initialization of SPI port
   SPI.setDataMode(SPI_MODE0); // configuration of SPI communication in mode 0
   SPI.setClockDivider(SPI_CLOCK_DIV16); // configuration of clock at 1MHz
   pinMode(SS, OUTPUT);
-  while (!Serial) {
-    ;
-  }
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present, not continuing");
-    while (true);
-  }
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WiFi network.");
-    status = WiFi.begin(ssid, passwd);
-    delay(10000); 
-  }
-  server.begin();
-  printWiFiStatus();
+  connectToWifi();
+  client.setServer(broker, 1883);
 }
 
 int data, volume;
 DynamicJsonDocument doc(1024);
+String output;
+char testi[40];
 
 void loop() {
-  volume = MIC3_getSound();
-  data = ALS_getData();
+  for (int i = 0; i < 50000; i++) {  // Creating average of 100 samples
+    volume += MIC3_getSound();
+    data += ALS_getData();
+  }
+  volume = volume / 50000;
+  data = data / 50000;
   doc["volume"] = volume;
   doc["als"] = data;
-  Serial.print(volume);
-  Serial.print(" : ");
-  Serial.println(data);
-  delay(4000);
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWifi();
+  }
+  if (!client.connected()){
+    reconnect();
+  }
+  serializeJson(doc, testi);
+  client.publish("iot-2/evt/1/fmt/json", testi);
+}
+
+void connectToWifi() {
+  WiFi.begin(ssid, passwd);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000); 
+  }
+}
+
+void(* resetFunc) (void) = 0; // declare reset function @ address 0
+
+void reconnect() {
+  while (!client.connected()){
+    if (client.connect("d:ypessw:Arduino:1212", brokerUser, brokerPass)) {
+      // continue
+    } else {
+      resetFunc(); // If can't connect to cloud, this will reset the arduino.
+    }
+  }
 }
 
 int MIC3_getSound(void) {
   digitalWrite(SS, LOW);  //activate chip select
-  Serial.println(SPI.transfer(0));
   int sound = SPI.transfer(0) | (SPI.transfer(0) << 8); //reconstruct 12-bit data
   digitalWrite(SS, HIGH); //deactivate chip select
   return sound;
@@ -87,18 +100,4 @@ int ALS_getData(void) {
   int data = SPI.transfer(0) | (SPI.transfer(0) << 8); //reconstruct 12-bit data
   digitalWrite(CS, HIGH); //deactivate chip select
   return data;
-}
-
-void printWiFiStatus() {
- 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
 }
